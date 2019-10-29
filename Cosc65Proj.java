eab797d5cb8b8a14986987cefd5aa934b2156d3b
paulcamerino123 <pauljoshuacamerino@gmail.com>
2019-10-29 10:02:18 +0800

    initial commit

---
+++ Cosc65Proj.java
@@ -0,0 +1,90 @@
+/**
+ * Subject requirement project for COSC65
+ * Create an attempt to copy an image using JApplet
+ * Image chosen from the animation Hunter x Hunter
+ * Character: Hisoka
+ */
+
+import javax.swing.JApplet;
+import java.awt.*;
+
+public class Cosc65Proj extends JApplet{
+    public void paint(Graphics g){
+    
+    
+    g.setColor(Color.red);
+    g.drawLine(70,0,110,230); //outer hair right
+
+    g.drawLine(110, 230, 125, 230); //upper ear hair right
+    
+    g.drawLine(110, 230, 100, 240); //ear right
+    g.drawLine(100, 240, 95, 250); //ear right
+    g.drawLine(95, 250, 105, 280); //ear right
+    g.drawLine(105, 280, 108, 290); //ear right
+    g.drawLine(108, 290, 112, 295); //ear right
+    g.drawLine(112, 295, 118, 300); //ear right
+    g.drawLine(118, 300, 125, 310); //ear right
+    g.drawLine(125, 310, 145, 320); //ear right
+
+    g.drawLine(125, 230, 132, 265); //sideburns right
+    g.drawLine(132, 265, 140, 130);  //sideburns right
+
+    g.drawLine(132, 265, 140, 300); //jawline right
+    g.drawLine(140, 300, 155, 345); //jawline right
+    g.drawLine(155, 345, 170, 370); //jawline right
+    g.drawLine(170, 370, 180, 390); //jawline right
+    g.drawLine(180, 390, 185, 400); //jawline right
+    g.drawLine(185, 400, 188, 405); //jawline right
+    g.drawLine(188, 405, 192, 415); //jawline right
+    g.drawLine(192, 415, 195, 420); //jawline right
+    g.drawLine(195, 420, 200, 430); //jawline right
+    g.drawLine(200, 430, 212, 445); //jawline right
+    
+    g.drawLine(212, 445, 216, 450); //chin
+    g.drawLine(216, 450, 220, 456); //chin
+    g.drawLine(220, 456, 225, 462); //chin
+    g.drawLine(225, 462, 262, 462); //chin
+
+    g.drawLine(262, 462, 270, 456); //jawline left
+    g.drawLine(270, 456, 275, 452); //jawline left
+    g.drawLine(275, 452, 280, 446); //jawline left
+    g.drawLine(280, 446, 285, 440); //jawline left
+    g.drawLine(285, 440, 288, 435); //jawline left
+    g.drawLine(288, 435, 291, 430); //jawline left
+    g.drawLine(291, 430, 295, 425); //jawline left
+    g.drawLine(295, 425, 300, 420); //jawline left
+    g.drawLine(300, 420, 305, 415); //jawline left
+    g.drawLine(305, 415, 310, 410); //jawline left
+    g.drawLine(310, 410, 320, 395); //jawline left
+    g.drawLine(320, 395, 325, 385); //jawline left
+    g.drawLine(325, 385, 329, 380); //jawline left
+    g.drawLine(329, 380, 335, 370); //jawline left
+    g.drawLine(335, 370, 340, 360); //jawline left
+    g.drawLine(340, 360, 350, 340); //jawline left
+    g.drawLine(350, 340, 355, 330); //jawline left
+    g.drawLine(355, 330, 360, 310); //jawline left
+    g.drawLine(360, 310, 365, 295); //jawline left
+    g.drawLine(365, 295, 370, 280); //jawline left
+    g.drawLine(370, 280, 377, 230); //jawline left
+
+    g.drawLine(140, 130, 260, 140); //forehead
+    g.drawLine(260, 140, 360, 130); //forehead
+
+    g.drawLine(150, 200, 175, 180); //eyebrows right
+    g.drawLine(175, 180, 230, 230); //eyebrows right
+
+    g.drawLine(360, 130, 370, 265); 
+
+    g.drawLine(370, 265, 377, 230); //sideburns left
+    g.drawLine(377, 230, 389, 230); //upper ear hair left
+
+    g.drawLine(389, 230, 402, 250); //ear left
+    g.drawLine(402, 250, 400, 270); //ear left
+    g.drawLine(400, 270, 395, 290); //ear left
+    g.drawLine(395, 290, 390, 300); //ear left
+    g.drawLine(390, 300, 360, 310); //ear left
+
+    g.drawLine(389, 230, 420, 0); //outer hair left
+    }
+
+}
